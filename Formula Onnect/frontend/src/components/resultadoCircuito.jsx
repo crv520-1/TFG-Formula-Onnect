86b9eba2154899a757922237b5cfcab2fe47d875
crv520-1 <crv520@inlumine.ual.es>
2025-03-18 21:00:34 +0100

    back, falta el front

--- Formula Onnect/frontend/src/components/resultadoCircuito.jsx
+++ Formula Onnect/frontend/src/components/resultadoCircuito.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from "react-router-dom";
 import { getImagenCircuito } from './mapeoImagenes.js';
 
-export const resultadoCircuito = () => {
+export const ResultadoCircuito = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { circuitId, year, round } = location.state || {};
@@ -14,38 +14,41 @@ export const resultadoCircuito = () => {
   useEffect(() => {
     const fetchData = async () => {
         try {
-            const circuitosResponse = await axios.get(`http://localhost:3000/api/circuitos`);
-            const circuito = circuitosResponse.data.find(circuito => circuito.circuitId === circuitId);
-            setCircuitos(circuito);
-            const resultadosResponse = await axios.get(`https://api.jolpi.ca/ergast/f1/${year}/${round}/results.json`);
-            const resultadosData = resultadosResponse.data.MRData.RaceTable.Races.Results;
-            resultadosData.forEach(resultado => {
-                posiciones.push({
-                    position: resultado.position,
-                    driver: resultado.Driver.givenName + " " + resultado.Driver.familyName, //Esto o cambiar por driverId para luego consultar en mi bd por el driverId para obtener los datos de nombre y apellidos del piloto
-                    team: resultado.Constructor.name,
-                    constructorId: resultado.Constructor.constructorId,
-                    puntos: resultado.points,
-                    status: resultado.status
-                });
-            });
+          const circuitosResponse = await axios.get(`http://localhost:3000/api/circuitos`);
+          const circuito = circuitosResponse.data.find(circuito => circuito.circuitId === circuitId);
+          if (!circuito) {
+            console.error("Circuito no encontrado");
+            return;
+          }
+          setCircuitos(circuito);
 
-            const horariosGranPremioResponse = await axios.get(`https://api.jolpi.ca/ergast/f1/${year}/${round}/races/.json`);
-            const horariosGranPremioData = horariosGranPremioResponse.data.MRData.RaceTable.Races;
-            horariosGranPremioData.forEach(horario => {
-                horariosGranPremio.push({
-                    diaCarrera: horario.date,
-                    horaCarrera: horario.time,
-                    diaClasificacion: horario.date,
-                    horaClasificacion: horario.time,
-                    diaFP3: horario.date,
-                    horaFP3: horario.time,
-                    diaFP2: horario.date,
-                    horaFP2: horario.time,
-                    diaFP1: horario.date,
-                    horaFP1: horario.time
-                });
+          const resultadosResponse = await axios.get(`https://api.jolpi.ca/ergast/f1/${year}/${round}/results.json`);
+          const resultadosData = resultadosResponse.data.MRData.RaceTable.Races[0].Results;
+          resultadosData.forEach(resultado => {
+            posiciones.push({
+              position: resultado.position,
+              driver: resultado.Driver.givenName + " " + resultado.Driver.familyName, //Esto o cambiar por driverId para luego consultar en mi bd por el driverId para obtener los datos de nombre y apellidos del piloto
+              team: resultado.Constructor.name,
+              constructorId: resultado.Constructor.constructorId,
+              puntos: resultado.points,
+              status: resultado.status
             });
+          });
+
+          const horariosGranPremioResponse = await axios.get(`https://api.jolpi.ca/ergast/f1/${year}/${round}/races.json`);
+          const horariosGranPremioData = horariosGranPremioResponse.data.MRData.RaceTable.Races[0];
+          horariosGranPremio.push({
+            diaCarrera: horariosGranPremioData.date ? horariosGranPremioData.date : "N/A fecha",
+            horaCarrera: horariosGranPremioData.time ? horariosGranPremioData.time : "N/A hora",
+            diaClasificacion: horariosGranPremioData.Qualifying.date ? horariosGranPremioData.Qualifying.date : "N/A fecha",
+            horaClasificacion: horariosGranPremioData.Qualifying.time ? horariosGranPremioData.Qualifying.time : "N/A hora",
+            diaFP3: horariosGranPremioData.ThirdPractice ? horariosGranPremioData.ThirdPractice.date : horariosGranPremioData.SprintQualifying ? horariosGranPremioData.Sprint.date : "N/A fecha",
+            horaFP3: horariosGranPremioData.ThirdPractice ? horariosGranPremioData.ThirdPractice.time : horariosGranPremioData.SprintQualifying ? horariosGranPremioData.Sprint.time : "N/A hora",
+            diaFP2: horariosGranPremioData.SecondPractice ? horariosGranPremioData.SecondPractice.date : horariosGranPremioData.SprintQualifying ? horariosGranPremioData.SprintQualifying.date : "N/A fecha",
+            horaFP2: horariosGranPremioData.SecondPractice ? horariosGranPremioData.SecondPractice.time : horariosGranPremioData.SprintQualifying ? horariosGranPremioData.SprintQualifying.time : "N/A hora",
+            diaFP1: horariosGranPremioData.FirstPractice ? horariosGranPremioData.FirstPractice.date : "N/A fecha",
+            horaFP1: horariosGranPremioData.FirstPractice ? horariosGranPremioData.FirstPractice.time : "N/A hora",
+          });
         } 
         catch (error) {
             console.error("Error en la API", error);
@@ -59,9 +62,9 @@ export const resultadoCircuito = () => {
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
         <h2 style={{ backgroundColor: "#C40000", borderRadius:"0.5vh", width: "15vh", fontSize:"2vh", textAlign: "center" }}>Calendario</h2>
       </div>
-      <img src={getImagenCircuito(circuitos.circuitId)} alt={circuitos.circuitName} />
+      <img src={getImagenCircuito(circuitos.circuitId)} alt={circuitos.circuitId} />
     </div>
   )
 }
 
-export default resultadoCircuito
+export default ResultadoCircuito
