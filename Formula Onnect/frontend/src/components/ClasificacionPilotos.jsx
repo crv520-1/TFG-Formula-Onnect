767cc2710222f98c07fdcd10da4b9e39fc039dac
crv520-1 <crv520@inlumine.ual.es>
2025-03-23 14:48:54 +0100

    Implementación de la clasificación de pilotos

--- Formula Onnect/frontend/src/components/ClasificacionPilotos.jsx
+++ Formula Onnect/frontend/src/components/ClasificacionPilotos.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { getPaisISO } from '../../../backend/scripts/mapeoPaises';
+import { getImagenEquipo, getImagenPiloto } from './mapeoImagenes';
 
 export const Clasificacion = () => {
   const navigate = useNavigate();
+  const [year, setYear] = useState(2025);
+  const [clasificacion, setClasificacion] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const clasificacionResponse = await axios.get(`https://api.jolpi.ca/ergast/f1/${year}/driverstandings/`);
+        const clasificacionData = clasificacionResponse.data.MRData.StandingsTable.StandingsLists[0];
+        const standings = clasificacionData.DriverStandings.map(piloto => ({
+          position: piloto.position,
+          points: piloto.points,
+          driverId: piloto.Driver.driverId,
+          nombre: piloto.Driver.givenName,
+          apellido: piloto.Driver.familyName,
+          nacionalidad: getPaisISO(piloto.Driver.nationality),
+          numero: piloto.Driver.permanentNumber,
+          constructorName: piloto.Constructors[0].name,
+          constructorId: piloto.Constructors[0].constructorId
+        }));
+        setClasificacion(standings); // Actualizas el estado una sola vez
+        console.log(standings);
+      } catch (error) {
+        console.error("Error en la API", error);
+      }
+    };
+    fetchData();
+  }, [year]);
 
   const handleEquipos = (e) => {
     e.preventDefault();
@@ -10,11 +40,45 @@ export const Clasificacion = () => {
     console.log("Equipos");
   }
 
+  // Se crea un array que comprenda los años entre el 2000 y el 2025
+  const years = Array.from({ length: 26 }, (_, i) => 2025 - i);
+
   return (
-    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
-      <h2 style={{ backgroundColor: "#C40000", borderRadius:"0.5vh", width: "15vh", fontSize:"2vh", textAlign: "center" }}>Pilotos</h2>
-      <button type='submit' onClick={handleEquipos} style={{ fontSize: "2vh", height:"3vh", marginLeft: "35vh", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor:"#15151E" }}>Equipos</button>
-    </div> 
+    <div style={{ display: "flex", flexDirection: "column", maxHeight: "98vh", overflow: "auto" }}>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
+        <h2 style={{ backgroundColor: "#C40000", borderRadius:"0.5vh", width: "15vh", fontSize:"2vh", textAlign: "center" }}>Pilotos</h2>
+        <button type='submit' onClick={handleEquipos} style={{ fontSize: "2vh", height:"3vh", marginLeft: "35vh", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor:"#15151E", border: "none" }}>Equipos</button>
+      </div>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+        {/* Seleccionable para elegir el año */}
+        <select style={{ borderRadius: "2vh", margin: "1vh", padding: "1vh", border:"none", backgroundColor:"#2C2C2C", color:"white", fontSize:"1.25vh" }} onChange={(e) => setYear(e.target.value)} value={year}>
+          {years.map(year => ( <option key={year} value={year}>{year}</option> ))}
+        </select>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", maxHeight: "100%", overflow: "auto", padding:"2vh" }}>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "2vh" }}>
+          {clasificacion.map(piloto => (
+            <div key={piloto.driverId} style={{ display: "grid", gridTemplateColumns: "1fr auto 1fr", alignItems: "center", backgroundColor:"#2c2c2c", borderRadius:"1vh", gap: "1vh", width:"24vw" }}>
+              <div style={{ display: "flex", flexDirection: "column", paddingLeft: "1vw" }}>
+                <span style={{ fontSize: "3vh", color: "white", textAlign: "center" }}>{piloto.position ? piloto.position : "-"}. </span>
+                <span style={{ fontSize: "2vh", color: "white", textAlign: "center" }}>{piloto.nombre}</span>
+                <span style={{ fontSize: "2vh", color: "white", textAlign: "center" }}>{piloto.apellido}</span>
+                <span style={{ fontSize: "2vh", color: "white", textAlign: "center" }}>{piloto.constructorName}</span>
+              </div>
+              <img src={getImagenPiloto(piloto.driverId)} alt="Foto de piloto" style={{ width: "15vh", height: "15vh", objectFit:"contain", paddingTop:"1vh" }} />
+              <div style={{ display: "flex", flexDirection: "column", paddingRight: "1vw" }}>
+                <span style={{ fontSize: "2vh", color: "white", textAlign: "center" }}>#{piloto.numero} </span>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                  <img src={getImagenEquipo(piloto.constructorId)} alt="Foto de piloto" style={{ width: "5vh", height: "5vh", objectFit:"contain", paddingRight:"1vw" }} />
+                  <img src={`https://flagcdn.com/w160/${piloto.nacionalidad}.png`} alt={piloto.nacionalidad} style={{ width: "5vh", height: "5vh", objectFit:"contain" }} />
+                </div>
+                <span style={{ fontSize: "3vh", color: "white", textAlign: "center" }}>{piloto.points} PTS.</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
