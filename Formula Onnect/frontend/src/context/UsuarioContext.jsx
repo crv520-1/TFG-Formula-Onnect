0ac377989eb77a3993e7260c7760ab5d9acbc20f
crv520-1 <crv520@inlumine.ual.es>
2025-03-24 13:36:42 +0100

    Persiste la recarga

--- Formula Onnect/frontend/src/context/UsuarioContext.jsx
+++ Formula Onnect/frontend/src/context/UsuarioContext.jsx
@@ -1,19 +1,37 @@
 // Contexto para poder pasar el id del usuario desde IniciarSession.jsx al resto de vistas a las que se accede desde el Dashboard.
-import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { createContext, useState } from 'react';
 
 export const UsuarioContext = createContext();
 
 export const UsuarioProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    // Recuperar el usuario del localStorage al cargar la aplicación
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const updateUser = (userId) => {
-      setUser(userId);
-      console.log("User updated:", userId);
+    setUser(userId);
+    // Guardar el usuario en localStorage
+    localStorage.setItem('user', JSON.stringify(userId));
+    console.log("User updated:", userId);
+  };
+
+  const clearUser = () => {
+    setUser(null);
+    // Eliminar el usuario del localStorage
+    localStorage.removeItem('user');
+    console.log("User cleared");
   };
 
   return (
-      <UsuarioContext.Provider value={{ user, setUser: updateUser }}>
-          {children}
-      </UsuarioContext.Provider>
+    <UsuarioContext.Provider value={{ user, setUser: updateUser, clearUser }}>
+      {children}
+    </UsuarioContext.Provider>
   );
+};
+
+UsuarioProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
