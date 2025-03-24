52c4ea08aab08bc1b39055ad7e0f84a559099002
crv520-1 <crv520@inlumine.ual.es>
2025-03-24 15:29:15 +0100

    comentarios model modificado y arreglado

--- Formula Onnect/backend/models/comentariosModel.js
+++ Formula Onnect/backend/models/comentariosModel.js
@@ -1,26 +1,43 @@
 const db = require('../config/db');
 
 // Obtener todos los comentarios
-exports.getAllComentarios = (callback) => {
-  const query = 'SELECT * FROM Comentarios';
-  db.query(query, callback);
+exports.getAllComentarios = async () => {
+  try {
+    const [rows] = await db.query('SELECT * FROM Comentarios');
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Obtener un comentario por su id
-exports.getComentariosByIdComentario = (idComentarios, callback) => {
-  const query = 'SELECT * FROM Comentarios WHERE idComentarios = ?';
-  db.query(query, [idComentarios], callback);
+exports.getComentariosByIdComentario = async (idComentarios) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM Comentarios WHERE idComentarios = ?', [idComentarios]);
+    return rows[0];
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Obtener los comentarios de una publicacion por su id
-exports.getComentariosByIdPublicacion = (post, callback) => {
-  const query = 'SELECT * FROM Comentarios WHERE post = ?';
-  db.query(query, [post], callback);
+exports.getComentariosByIdPublicacion = async (post) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM Comentarios WHERE post = ?', [post]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Crear un nuevo comentario
-exports.createComentarios = (comentario, callback) => {
-  const query = 'INSERT INTO Comentarios (idComentarios, text, user, post) VALUES (?, ?, ?, ?)';
+exports.createComentarios = async (comentario) => {
+  const query = 'INSERT INTO Comentarios (text, user, post) VALUES (?, ?, ?)';
   const values = [comentario.text, comentario.user, comentario.post];
-  db.query(query, values, callback);
+  try {
+    const [result] = await db.query(query, values);
+    return result.insertId;
+  } catch (error) {
+    throw error;
+  }
 };
