2eeb767aea0b2c630edfce9729ee2f6dd11a47a9
crv520-1 <crv520@inlumine.ual.es>
2025-04-05 19:45:11 +0200

    Supongo que prueba y error

--- Formula Onnect/frontend/src/components/HeaderPerfil.jsx
+++ Formula Onnect/frontend/src/components/HeaderPerfil.jsx
@@ -2,6 +2,7 @@ import { ArrowRightEndOnRectangleIcon, PencilSquareIcon } from "@heroicons/react
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { useNavigate } from "react-router-dom";
+import '../styles/HeaderPerfil.css';
 
 const PerfilHeader = ({ usuario, numeroPublicaciones, seguidores, siguiendo, sigo, idUser, mismoUsuario, onSeguidoresChange }) => {
   const navigate = useNavigate();
@@ -39,43 +40,48 @@ const PerfilHeader = ({ usuario, numeroPublicaciones, seguidores, siguiendo, sig
   }
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-        <h1 style={{ fontSize: "4vh", textAlign: "center" }}> {usuario.nickName} </h1>
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-            <img src={usuario.fotoPerfil} alt="Foto de perfil" style={{ width: "15vh", height: "15vh", borderRadius: "50%", color:"white", backgroundColor:"white" }} />
-            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                    <p style={{ fontSize: "3vh", marginLeft: "1vh", margin: "1vh" }}>{usuario.nombreCompleto}</p>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                        {mismoUsuario ? (
-                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                                <button type='submit' onClick={handleEditarPerfil} style={{ fontSize: "2vh", marginLeft: "1vh", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", height:"3vh", border: "none", backgroundColor:"#15151E" }}><PencilSquareIcon style={{ width: "2vh", height: "2vh" }} /></button>
-                                <button type='submit' onClick={handleCerrarSesion} style={{ fontSize: "2vh", marginLeft: "1vh", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", height:"3vh", border: "none", backgroundColor:"#15151E" }}><ArrowRightEndOnRectangleIcon style={{ width: "2vh", height: "2vh" }} /></button>
-                            </div>
-                        ) : null}
+    <div className="perfil-header">
+        <h1 className="perfil-header__nickname">{usuario.nickName}</h1>
+        <div className="perfil-header__info">
+            <img src={usuario.fotoPerfil} alt="Foto de perfil" className="perfil-header__image" />
+            <div className="perfil-header__details">
+                <div className="perfil-header__name-buttons">
+                    <p className="perfil-header__fullname">{usuario.nombreCompleto}</p>
+                    {mismoUsuario && (
+                        <div className="perfil-header__name-buttons">
+                            <button className="perfil-header__button" onClick={handleEditarPerfil}>
+                                <PencilSquareIcon className="perfil-header__button-icon" />
+                            </button>
+                            <button className="perfil-header__button" onClick={handleCerrarSesion}>
+                                <ArrowRightEndOnRectangleIcon className="perfil-header__button-icon" />
+                            </button>
+                        </div>
+                    )}
+                </div>
+                <div className="perfil-header__stats">
+                    <div className="perfil-header__stat">
+                        <p>Publicaciones</p>
+                        <p>{numeroPublicaciones}</p>
+                    </div>
+                    <div className="perfil-header__stat">
+                        <p>Seguidores</p>
+                        <p>{seguidores}</p>
+                    </div>
+                    <div className="perfil-header__stat">
+                        <p>Siguiendo</p>
+                        <p>{siguiendo}</p>
                     </div>
                 </div>
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft: "2vh" }}>
-                        <p style={{ fontSize: "1.5vh", margin: "0.3vh 0" }}> Publicaciones </p>
-                        <p style={{ fontSize: "1.5vh", margin: "0" }}> {numeroPublicaciones} </p>
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft: "2vh" }}>
-                        <p style={{ fontSize: "1.5vh", margin: "0.3vh 0" }}> Seguidores </p>
-                        <p style={{ fontSize: "1.5vh", margin: "0" }}> {seguidores} </p>
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft: "2vh" }}>
-                        <p style={{ fontSize: "1.5vh", margin: "0.3vh 0" }}> Siguiendo </p>
-                        <p style={{ fontSize: "1.5vh", margin: "0" }}> {siguiendo} </p>
-                    </div>
-                </div>
-                {!mismoUsuario ? (
-                    !sigo ? (
-                        <button type='submit' onClick={handleSeguir} style={{ fontSize: "2vh", height:"3vh", marginTop: "1vh", marginLeft:"1vw", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", border: "none", backgroundColor:"#C40000", width:"15vw" }}> Seguir </button>
-                    ) : (
-                        <button type='submit' onClick={handleSeguir} style={{ fontSize: "2vh", height:"3vh", marginTop: "1vh", marginLeft:"1vw", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", border: "none", backgroundColor:"white", width:"15vw", color:"#C40000" }}> Dejar de seguir </button>
-                    )
-                ) : null}
+                {!mismoUsuario && (
+                    <button
+                        className={`perfil-header__follow-button ${
+                            sigo ? 'perfil-header__follow-button--dejar-seguir' : 'perfil-header__follow-button--seguir'
+                        }`}
+                        onClick={handleSeguir}
+                    >
+                        {sigo ? 'Dejar de seguir' : 'Seguir'}
+                    </button>
+                )}
             </div>
         </div>
     </div>
