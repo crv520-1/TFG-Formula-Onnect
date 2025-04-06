f7c202c0acd160297431365e35578b5ceda8cfd9
crv520-1 <crv520@inlumine.ual.es>
2025-04-06 16:33:25 +0200

    Fuera css

--- Formula Onnect/frontend/src/components/ClasificacionEquipos.jsx
+++ Formula Onnect/frontend/src/components/ClasificacionEquipos.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { getPaisISO } from '../../../backend/scripts/mapeoPaises';
 import { carga } from './animacionCargando';
-import './clasificacionEquipos.css';
 import { getImagenEquipo, getLivery } from './mapeoImagenes';
 
 export const ClasificacionEquipos = () => {
@@ -68,36 +67,35 @@ export const ClasificacionEquipos = () => {
   if (cargando) { return ( carga() )};
     
   return (
-    <div className="container">
-      <div className="header">
-        <button type='submit' onClick={handlePilotos} className="button-pilotos">Pilotos</button>
-        <h2 className="title-equipos">Equipos</h2>
+    <div style={{ display: "flex", flexDirection: "column", maxHeight: "98vh", overflow: "auto" }}>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
+        <button type='submit' onClick={handlePilotos} style={{ fontSize: "2vh", height:"3vh", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor:"#15151E", border: "none" }}>Pilotos</button>
+        <h2 style={{ backgroundColor: "#C40000", borderRadius:"0.5vh", width: "15vh", fontSize:"2vh", textAlign: "center", marginLeft: "35vh" }}>Equipos</h2>
       </div> 
-      <div className="header">
-        <select className="select-year" onChange={(e) => setYear(e.target.value)} value={year}>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+        {/* Seleccionable para elegir el año */}
+        <select style={{ borderRadius: "2vh", margin: "1vh", padding: "1vh", border:"none", backgroundColor:"#2C2C2C", color:"white", fontSize:"1.25vh" }} onChange={(e) => setYear(e.target.value)} value={year}>
           {years.map(year => ( <option key={year} value={year}>{year}</option> ))}
         </select>
       </div>
-      <div className="grid-container">
-        <div className="grid">
+      <div style={{ display: "flex", flexDirection: "column", maxHeight: "100%", overflow: "auto", padding:"2vh" }}>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "2vh" }}>
           {clasificacion.map((equipo, index) => (
-            <div key={equipo.constructorId} className={`card ${clasificacion.length % 2 !== 0 && index === clasificacion.length - 1 ? "card-single" : ""}`}>
-              <div className="card-content">
-                <div className="card-header">
-                  <span className="card-position" style={{ color: getColorPosicion(Number(equipo.position)), fontWeight: getEstiloPosicion(Number(equipo.position)) }}>
-                    {equipo.position ? equipo.position : "-"}. 
-                  </span>
-                  <span className="card-name">{equipo.nombre}</span>
+            <div key={equipo.constructorId} style={{ display: "grid", gridTemplateColumns: "1fr auto 1fr", alignItems: "center", backgroundColor:"#2c2c2c", borderRadius:"1vh", gap: "1vh", width:"22vw", justifyContent: "space-between",  ...(clasificacion.length % 2 !== 0 && index === clasificacion.length - 1 ? { gridColumn: "1 / span 2", justifySelf: "center" } : {}) }}>
+              <div style={{ display: "flex", flexDirection: "column", paddingLeft: "0.5vw" }}>
+                <div style={{ display: "flex", flexDirection: "row", paddingLeft: "1vw" }}>
+                  <span style={{ fontSize: "2.5vh", color: getColorPosicion(Number(equipo.position)), textAlign: "center", paddingBottom:"1vh", fontWeight:getEstiloPosicion(Number(equipo.position)) }}>{equipo.position ? equipo.position : "-"}. </span>
+                  <span style={{ fontSize: "2vh", color: "white", textAlign: "center", paddingLeft:"0.25vw", paddingTop:"0.5vh" }}>{equipo.nombre}</span>
                 </div>
-                <div className="card-flags">
-                  <img src={getImagenEquipo(equipo.constructorId)} alt="Foto de equipo" className="card-flag" />
-                  <img src={`https://flagcdn.com/w160/${equipo.nacionalidad}.png`} alt={equipo.nacionalidad} className="card-flag" />
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", paddingLeft: "1vw"}}>
+                  <img src={getImagenEquipo(equipo.constructorId)} alt="Foto de equipo" style={{ width: "5vh", height: "5vh", objectFit:"contain", paddingRight:"1vw" }} />
+                  <img src={`https://flagcdn.com/w160/${equipo.nacionalidad}.png`} alt={equipo.nacionalidad} style={{ width: "5vh", height: "5vh", objectFit:"contain" }} />
                 </div>
-                <div>
-                  <span className="card-points">{equipo.points} PTS.</span>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                  <span style={{ fontSize: "3vh", color: "white", textAlign: "center" }}>{equipo.points} PTS.</span>
                 </div>
               </div>
-              <img src={getLivery(equipo.constructorId)} alt="Foto de equipo" className="card-livery" />
+              <img src={getLivery(equipo.constructorId)} alt="Foto de equipo" style={{ width: "25vh", height: "15vh", objectFit:"contain", paddingLeft:"1.5vw", borderRadius:"1vh" }} />
             </div>
           ))}
         </div>
