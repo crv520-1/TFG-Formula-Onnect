8884835173244f9ec850f8d760830b6ab7149d80
crv520-1 <crv520@inlumine.ual.es>
2025-04-13 19:34:53 +0200

    Todos los datos con estilo similar

--- Formula Onnect/frontend/src/components/DatosPiloto.jsx
+++ Formula Onnect/frontend/src/components/DatosPiloto.jsx
@@ -87,18 +87,17 @@ export const DatosPiloto = () => {
   if (cargando) { return carga(); }
 
   return (
-    <div className='container_columna_marginTop'>
-      <div className='container_fila'>
+    <div className='container_overflow'>
+      <div className='container_fila_paddingTop_v5'>
         <button type='submit' onClick={handlePilotos} className='boton_fondo_c4_v2'>Pilotos</button>
         <button type='submit' onClick={handleEquipos} className='boton_fondo_15_v4'>Equipos</button>
         <button type='submit' onClick={handleCircuitos} className='boton_fondo_15_v4'>Circuitos</button>
       </div>
       <br />
-      <div className='container_columna_2c_v4'>
+      <div className='container_overflow_border'>
         <div className='container_fila_2c'>
           <p className='datos_v7'>{pilotos.nombrePiloto} {pilotos.apellidoPiloto}</p>
         </div>
-        <div className='container_fila_marginBottom'>
           <div className='container_columna_marginBottom_v2'>
             <img src={getImagenPiloto(pilotos.driverId)} alt="Imagen del piloto" className='imagen_piloto_v2'/>
             <div className='container_fila_marginBottom'>
@@ -110,65 +109,65 @@ export const DatosPiloto = () => {
             {driverData && (
               <div className='container_columna_blanca'>
                 <div className='container_fila_paddingBottom'>
-                  <div className='container_columna_datos_v2'>
-                    <p className='datos_centrados'>Fecha de nacimiento:</p>
+                  <div className='container_columna_datos'>
+                    <p className='datos_centrados_v2'>Fecha de nacimiento:</p>
                     <p className='datos_centrados'>{driverData.birthDate}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
-                    <p className='datos_centrados'>Lugar de nacimiento:</p>
+                  <div className='container_columna_datos'>
+                    <p className='datos_centrados_v2'>Lugar de nacimiento:</p>
                     <p className='datos_centrados'>{driverData.birthPlace}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
-                    <p className='datos_centrados'>Fecha de fallecimiento:</p>
+                  <div className='container_columna_datos'>
+                    <p className='datos_centrados_v2'>Fecha de fallecimiento:</p>
                     <p className='datos_centrados'>{driverData.deathDate}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
-                    <p className='datos_centrados'>Lugar de fallecimiento:</p>
+                  <div className='container_columna_datos'>
+                    <p className='datos_centrados_v2'>Lugar de fallecimiento:</p>
                     <p className='datos_centrados'>{driverData.deathPlace}</p>
                   </div>
                 </div>
                 <div className='container_fila_paddingBottom'>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Primera carrera:</p>
                     <p className='datos_centrados'>{driverData.firstRace}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Primera Victoria:</p>
                     <p className='datos_centrados'>{driverData.firstWin}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Última carrera:</p>
                     <p className='datos_centrados'>{driverData.lastRace}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Carreras terminadas:</p>
                     <p className='datos_centrados'>{driverData.racesFinished}</p>
                   </div>
                 </div>
                 <div className='container_fila_paddingBottom'>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Victorias:</p>
                     <p className='datos_centrados'>{driverData.wins}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Poles:</p>
                     <p className='datos_centrados'>{driverData.poles}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Vueltas rápidas:</p>
                     <p className='datos_centrados'>{driverData.fastestLaps}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Podios:</p>
                     <p className='datos_centrados'>{driverData.podiums}</p>
                   </div>
                 </div>
                 <div className='container_fila_paddingBottom'>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Mundiales:</p>
                     <p className='datos_centrados'>{driverData.championships}</p>
                   </div>
-                  <div className='container_columna_datos_v2'>
+                  <div className='container_columna_datos'>
                     <p className='datos_centrados'>Puntos totales:</p>
                     <p className='datos_centrados'>{driverData.points}</p>
                   </div>
@@ -176,7 +175,6 @@ export const DatosPiloto = () => {
               </div>
             )}
           </div>
-        </div>
       </div>
     </div>
   )
